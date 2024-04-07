32eb8238431385ff0cb2339ee3dc95dfab193dd8
Karl Johan Thomsen <kthom20@student.sdu.dk>
2024-04-07 11:01:53 +0200

    Implemented asteroids system

---
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidSplitter.java
@@ -0,0 +1,14 @@
+package dk.sdu.mmmi.cbse.asteroid;
+
+import dk.sdu.mmmi.cbse.common.asteroids.IAsteroidSplitter;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.World;
+
+
+public class AsteroidSplitter implements IAsteroidSplitter {
+
+    @Override
+    public void createSplitAsteroid(Entity e, World world) {
+    }
+
+}
